57b3d7a695369df6950a0a8e9d66cb39968677cc
AliciaMonsibais <alicia.monsibais1@gmail.com>
2019-08-27 11:15:24 -0500

    small edits

--- src/main/databases/ads_seeder.sql
+++ src/main/databases/ads_seeder.sql
@@ -5,4 +5,4 @@ TRUNCATE users;
 
 INSERT INTO users (username, email, password) VALUES ('user1', 'creativemail@email.com', 'password');
 
-INSERT INTO ads (user_id, title, description) VALUES (1, 'Creative Title', 'Look at this awesome, lightly used item!')
+INSERT INTO ads (user_id, title, description) VALUES (1, 'Creative Title', 'Look at this awesome, lightly used item!');
